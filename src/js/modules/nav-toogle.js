5d72faa25acc346ccb5ac8735e134e69061c4680
Nikimad <PybePoiD@gmail.com>
2022-08-12 22:45:10 +0300

    add demo footer layout

--- src/js/modules/nav-toogle.js
+++ src/js/modules/nav-toogle.js
@@ -1,3 +1,12 @@
-const toogler = document.querySelector('.mobile-nav-toogle');
+const primaryHeader = document.querySelector('.prim-header');
+const navToggle = document.querySelector('.mobile-nav-toogle');
+const primaryNav = document.querySelector('.prim-nav');
 
-export default toogler.addEventListener('click', () => toogler.classList.toggle('open'));
+
+
+navToggle.addEventListener('click', () => {
+    navToggle.classList.toggle('open');
+    navToggle.setAttribute('aria-expanded', !primaryNav.hasAttribute('data-visible'));
+    primaryNav.toggleAttribute("data-visible");
+    primaryHeader.toggleAttribute("data-overlay");
+});
